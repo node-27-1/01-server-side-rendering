d0b5f52da2f9680b6bdbcec1e981a8320a809c76
Andres Mendoza <andres.david.mm@hotmail.com>
2023-08-28 21:49:48 -0500

    first commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,38 @@
 const express = require('express');
+const users = require('../users.json');
 const router = express.Router();
 
 // colocar las rutas aquí
 router.get('/', (req, res) => {
-    res.send("Welcome to express");
+    res.render("portfolio");
 });
+
+router.get('/about-me', (req, res) => {
+    res.render("about-me");
+});
+
+router.get('/projects', (req, res) => {
+    res.render("projects");
+});
+
+router.get('/projects/:id', (req, res) => {
+    const { id } = req.params;
+    res.render("project-detail", { projectId: id });
+});
+
+router.get('/services', (req, res) => {
+    res.render("services");
+});
+
+router.get('/team', (req, res) => {
+
+    res.render("team", { users });
+});
+
+router.get('/team/:name', (req, res) => {
+    const { name } = req.params;
+    res.render('team-member', { name });
+})
 
 
 module.exports = router;
